dde3dff851cfd3b538d7b4892246777dcaf06891
twennyforkay <notcandymamtruth@gmail.com>
2023-06-27 21:34:14 +0200

    tHE NEW ONE

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function add (){
 let opt1 =document.getElementById("opt1").value;
 let opt2 =document.getElementById("opt2").value;
 let answer =document.getElementById("answer");
-opt1=Number(opt1)
-opt2=Number(opt2)
+opt1=Number(opt1);
+opt2=Number(opt2);
 answer.innerHTML=opt1+opt2;
-console.log(opt1, opt2, answer)
+console.log(opt1, opt2, answer);
 }
 
 function clearAnswer(){
@@ -29,19 +29,19 @@ answer.innerHTML='';
 
 
 function minus (){
-    let opt3 =document.getElementById("opt3").value;
-    let opt4 =document.getElementById("opt4").value;
+    let opt5 =document.getElementById("opt3").value;
+    let opt6 =document.getElementById("opt4").value;
     let answer =document.getElementById("answer2");
-    opt1=Number(opt3)
-    opt2=Number(opt4)
+    opt1=Number(opt3);
+    opt2=Number(opt4);
     answer.innerHTML=opt3-opt4;
-    console.log(opt3, opt4, answer2)
+    console.log(opt3, opt4, answer2);
     }
     
     function clearAnswer(){
         
-    let opt3=document.getElementById("opt3").value
-    let opt4=document.getElementById("opt4").value;
+    let opt5=document.getElementById("opt3").value;
+    let opt6=document.getElementById("opt4").value;
     console.log(opt3, opt4);
     if(!opt3|| !opt4){
     let answer =document.getElementById("answer2");
@@ -53,3 +53,31 @@ function minus (){
     
     
     }
+
+
+
+    function multiply (){
+        let opt5 =document.getElementById("opt5").value;
+        let opt6 =document.getElementById("opt6").value;
+        let answer =document.getElementById("answer3");
+        opt5=Number(opt5);
+        opt6=Number(opt6);
+        answer.innerHTML=opt5-opt6;
+        console.log(opt5, opt6, answer3);
+        }
+        
+        function clearAnswer(){
+            
+        let opt5=document.getElementById("opt5").value;
+        let opt6=document.getElementById("opt6").value;
+        console.log(opt5, opt6);
+        if(!opt5|| !opt6){
+        let answer =document.getElementById("answer3");
+        answer.innerHTML='';
+        }
+        
+        
+        
+        
+        
+        }

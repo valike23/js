1f4db110c8aa1d328455adeb984baeff7f8c8ae1
valike23 <ludike23@gmail.com>
2023-06-30 19:12:48 +0100

    init

--- index.js
+++ index.js
@@ -1,83 +1,48 @@
-function add (){
-let opt1 =document.getElementById("opt1").value;
-let opt2 =document.getElementById("opt2").value;
-let answer =document.getElementById("answer");
-opt1=Number(opt1);
-opt2=Number(opt2);
-answer.innerHTML=opt1+opt2;
-console.log(opt1, opt2, answer);
+function add() {
+    let opt1 = document.getElementById("opt1").value;
+    let opt2 = document.getElementById("opt2").value;
+    let answer = document.getElementById("answer");
+    opt1 = Number(opt1);
+    opt2 = Number(opt2);
+    answer.innerHTML = opt1 + opt2;
+    console.log(opt1, opt2, answer);
 }
 
-function clearAnswer(){
-    
-let opt1=document.getElementById("opt1").value;
-let opt2=document.getElementById("opt2").value;
-console.log(opt1, opt2);
-if(!opt1|| !opt2){
-let answer =document.getElementById("answer");
-answer.innerHTML='';
-}
+function clearAnswer() {
 
-
-
-
-
-}
-
-
-
-
-
-function minus (){
-    let opt5 =document.getElementById("opt3").value;
-    let opt6 =document.getElementById("opt4").value;
-    let answer =document.getElementById("answer2");
-    opt1=Number(opt3);
-    opt2=Number(opt4);
-    answer.innerHTML=opt3-opt4;
-    console.log(opt3, opt4, answer2);
-    }
-    
-    function clearAnswer(){
-        
-    let opt5=document.getElementById("opt3").value;
-    let opt6=document.getElementById("opt4").value;
-    console.log(opt3, opt4);
-    if(!opt3|| !opt4){
-    let answer =document.getElementById("answer2");
-    answer.innerHTML='';
-    }
-    
-    
-    
-    
-    
+    let opt1 = document.getElementById("opt1").value;
+    let opt2 = document.getElementById("opt2").value;
+    console.log(opt1, opt2);
+    if (!opt1 || !opt2) {
+        let answer = document.getElementById("answer");
+        answer.innerHTML = '';
     }
 
 
 
-    function multiply (){
-        let opt5 =document.getElementById("opt5").value;
-        let opt6 =document.getElementById("opt6").value;
-        let answer =document.getElementById("answer3");
-        opt5=Number(opt5);
-        opt6=Number(opt6);
-        answer.innerHTML=opt5-opt6;
-        console.log(opt5, opt6, answer3);
-        }
-        
-        function clearAnswer(){
-            
-        let opt5=document.getElementById("opt5").value;
-        let opt6=document.getElementById("opt6").value;
-        console.log(opt5, opt6);
-        if(!opt5|| !opt6){
-        let answer =document.getElementById("answer3");
-        answer.innerHTML='';
-        }
-        
-        
-        
-        
-        
-        }
+
+
+}
+
+
+
+
+
+function minus() {
+    let opt3 = document.getElementById("opt3").value;
+    let opt4 = document.getElementById("opt4").value;
+    let answer = document.getElementById("answer2");
+    opt3 = Number(opt3);
+    opt4 = Number(opt4);
+    answer.innerHTML = opt3 - opt4;
+}
+
+function multiply() {
+    let opt5 = document.getElementById("opt5").value;
+    let opt6 = document.getElementById("opt6").value;
+    let answer = document.getElementById("answer3");
+    opt5 = Number(opt5);
+    opt6 = Number(opt6);
+    answer.innerHTML = opt5 * opt6;
+}
+
